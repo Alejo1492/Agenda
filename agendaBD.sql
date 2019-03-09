dbb75c175d7542f84599605f6c0bb05bcd2d9c31
Alejo1492 <Alejoudec92@gmail.com>
2019-03-09 18:43:11 -0500

    Correcciones
    Correccion de errores a la base de datos

--- agendaBD.sql
+++ agendaBD.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-09-2017 a las 19:48:06
+-- Tiempo de generación: 06-03-2019 a las 19:48:06
 -- Versión del servidor: 10.1.25-MariaDB
 -- Versión de PHP: 7.1.7
 
@@ -18,16 +18,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de datos: `agenda`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `evento`
---
-
 CREATE TABLE `evento` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
@@ -38,10 +28,6 @@ CREATE TABLE `evento` (
   `idusuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Volcado de datos para la tabla `evento`
---
-
 INSERT INTO `evento` (`id`, `title`, `fechainicio`, `horainicio`, `fechafin`, `horafin`, `idusuario`) VALUES
 (7, 'TOMAS', '2017-09-06', '05:30:00', '2017-09-06', '06:30:00', 1),
 (10, 'Prueba 2', '2017-09-08', '07:00:00', '0000-00-00', '00:00:00', 1),
@@ -49,11 +35,6 @@ INSERT INTO `evento` (`id`, `title`, `fechainicio`, `horainicio`, `fechafin`, `h
 (13, 'Juan 01 ', '2017-09-07', '05:00:00', '2017-09-07', '06:00:00', 2),
 (14, 'confirmacion', '2017-09-11', '07:00:00', '2017-09-11', '07:00:00', 2);
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
@@ -62,31 +43,15 @@ CREATE TABLE `usuarios` (
   `email` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `usuarios`
---
 
 INSERT INTO `usuarios` (`id`, `nombre`, `psw`, `email`) VALUES
 (1, 'Jordyn tomas', '123', 'urbinajordin@gmail.com'),
 (2, 'Juan ', '123', 'juan@gmail.com');
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `evento`
---
 ALTER TABLE `evento`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `evento`
---
 ALTER TABLE `evento`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;COMMIT;
 
